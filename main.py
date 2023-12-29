5449a2a65ced9bfb628a24b94791e8afe409a6cc
Austin Sekel <giants38914@gmail.com>
2023-12-28 19:59:40 -0500

    questions added

--- main.py
+++ main.py
@@ -4,4 +4,37 @@ class Question:
         self.answer = answer
 
 
-
+questions = [
+    Question(
+        "Which built-in Python function will return the length of an object?\n"
+        "A. float()\n"
+        "B. str()\n"
+        "C. len()\n"
+        "D. int()\n",
+        "C"
+    ),
+    Question(
+        "Which built-in JavaScript method merges two or more arrays?\n"
+        "A. .split()\n"
+        "B. .shift()\n"
+        "C. .join()\n"
+        "D. .concat()\n",
+        "D"
+    ),
+    Question(
+        "How do you say, 'I love plants & animals' in Spanish?\n"
+        "A. Me amo las verduras con las frutas\n"
+        "B. Me encanta las flores y los animales\n"
+        "C. Me encanta el calor y el frio\n"
+        "D. Me amo las gatos con carisma\n",
+        "B"
+    ),
+    Question(
+        "¿Cómo se dice 'aprender es divertido' en inglés?\n"
+        "A. learning is fun\n"
+        "B. apprenticing is dynamic\n"
+        "C. aprehending is doubtful\n"
+        "D. learning is discerning\n",
+        "A"
+    )
+]
